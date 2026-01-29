b0e847764fc3aae1bfea6b3d76218111b63cf017
황호윤 <hoyoon9227@gmail.com>
2026-01-29 22:50:31 +0900

    애플 이름 이메일 체크 비어있을때 오류나는걸로 수정

--- src/main/java/com/chwipoClova/user/service/UserService.java
+++ src/main/java/com/chwipoClova/user/service/UserService.java
@@ -320,7 +320,7 @@ public class UserService {
         String email = claims.get("email", String.class);
         Integer snsType = UserLoginType.APPLE.getCode();
 
-        if (StringUtils.isNotBlank(email) || StringUtils.isNotBlank(name)) {
+        if (StringUtils.isBlank(email) || StringUtils.isBlank(name)) {
             throw new CommonException(ExceptionCode.USER_NULL.getMessage(), ExceptionCode.USER_NULL.getCode());
         }
 
